8c979eab26f8a24c33f880e187ade5f4e7adc4cc
raimom <raimo.muurinen@gmail.com>
2015-05-29 00:00:17 +0300

    Ensimmäinen koodipätkä (harjoitus) pressiklubi-vaikuttavuus.js lisätty.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# gradublogi
+﻿# gradublogi
 Täältä löytyy Tampereen yliopiston Opiskelijablogissa käyttämäni koodiesimerkit.
+
+Ensimmäinen pressiklubi-vaikuttavuus.js -tiedosto on pienimuotoinen nopea harjoitus.
---
+++ pressiklubi-vaikuttavuus.js
@@ -0,0 +1,22 @@
+function osallisuus() {
+
+	if (kansalainen.vaikuttavuus.tunne > 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+
+
+var kansalainen.vaikuttavuus.tunne = {
+
+täysinSamaaMieltä:2,
+osittainSamaaMieltä:1,
+enOsaaSanoa:0,
+osittainEriMieltä:-1,
+täysinEriMieltä:-2
+
+};
